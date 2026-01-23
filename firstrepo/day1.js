db39be8a92e6e52b5ce39375f4ce979d7a71803b
coderxsteve <gaganpc314@gmail.com>
2026-01-23 15:09:34 +0530

    if else practice done day 1

---
+++ firstrepo/day1.js
@@ -0,0 +1,52 @@
+array =["1234"]
+console.log(Array) 
+ 
+let object ={
+usrname1: "dustin",
+username2: "steve"
+
+} 
+console.log(array  ==object)
+
+if( Array.isArray(object) ){
+console.log ("array hai")
+
+}  
+else{
+console.log("object hai")
+
+
+}
+
+console.log(typeof(array))
+console.log(typeof(object))
+
+
+const  users =["steve"  , "dustin" , "nance" ]
+const passwords=["6969","gagan","shamsher","eleven","mike"  ]
+
+console.log(users[2],users[1],users[0])
+
+console.log(passwords.at(-2),passwords.at(-1),passwords.at(-4))
+//   console.log(users.pop())
+// console.log(users)
+
+console.log(users.shift())
+console.log(users)
+
+console.log(users.unshift())
+console.log(users)
+console.log(users.shift())
+console.log(users)
+
+document.getElementById("users")
+
+
+
+
+
+
+
+
+
+
---
+++ firstrepo/ifelsepractice.js
@@ -0,0 +1,87 @@
+let= marks =75;
+
+if (marks>= 90){
+console.log("A grade");
+
+} else if (marks >= 60){
+console.log("B grade")
+
+} else{
+console.log("fail");
+
+}
+
+let age =  20;
+let hasid = true;
+
+if (age >= 18 && hasid){
+console.log("entry allowed");
+
+
+} else {
+console.log|("entry not allowed")
+
+}
+
+let tempreture = 30;
+
+if (tempreture> 30 ){
+console.log("hot");
+
+} else if (tempreture>=15 && tempreture <= 30){
+console.log("normal");
+    
+}else{
+console.log("cold");
+
+}
+
+let num = 0;
+
+if (num> 0){
+console.log("positive");
+
+} else if (num<0){
+console.log("negative");
+
+} else {
+console.log ("zero");
+
+}
+
+
+let age1 = 17;
+
+if (age1 >=17){
+console.log("adult");
+
+}else {
+console.log ("minor");
+
+
+}
+
+let time = 22;
+
+if (time >=5 && time <=11){
+console.log("good morning")
+
+} else if (time >=12 && time <=17 ){
+console.log("good afternoon")
+
+
+} else if (time>=18 && time <=21 ){
+console.log("good evening")
+
+
+}  else {
+console.log("good night")
+
+}
+
+
+
+
+
+
+
